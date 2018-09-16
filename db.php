7a785d6e568725339e9882eec6dd76d8d4afcd52
Susmit <susmit.py@gmail.com>
2018-09-16 14:28:35 +0530

    1

--- db.php
+++ db.php
@@ -1,6 +1,7 @@
 <?php
+//						server       username  password     db name
 $conn = mysqli_connect('localhost', 'susmit', '135792846', 'users');
-if (mysqli_connect_errno()){
+if (mysqli_connect_errno()){    //execute ? true : error
 	echo "Failed to connect". mysqli_connect_errno();
 }
 
